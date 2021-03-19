f5ce2af6eb3ce2a151a4ad75f0f44837f1b14f73
Richard-Ott <richard.ott1900@gmail.com>
2021-03-19 16:25:45 +0100

    half way towards building the first Cronus using version

--- postburial_prod.m
+++ postburial_prod.m
@@ -7,12 +7,9 @@
 % deposition, where deposition of a random amount of layers with certain
 % thickness and time intervals follow power law distributions, as suggested
 % by Gomez et al. 2002.
-% Calculates the attenuation length for spallation following CRONUS and
-% Sato (2008) with a dependency on time and air-pressure.
-% This code still uses exponentials for muons production. I'm having
-% problems understanding how to modify the Balco 2017 code to get
-% attenuation lengths for fast and slow muons.
-% The code is based on an R-script by M. Lupker.
+% The code in this branch uses CRONUS to determine production rates.
+% The code is based on an R-script by M. Lupker but is now heavily
+% modified.
 %
 % Richard Ott, 2020
 clc
@@ -20,24 +17,44 @@ clear
 close all
 
 addpath '.\subroutines'
-addpath '..\data'
+% addpath '..\data'
+addpath '..\..\..\..\Crete\Cretan_fans\data'
 
 % USER CHOICE ----------------------------------------------------------- %
 nuclide = '10Be';       % Choose '10Be' or '36Cl'
-export = 1;             % do you want to save figures and model.data
+export = 1;             % do you want to save figures and model data
 n = 5e3;                % number of runs
+scaling_model = 'st';   % choose your scaling model, nomenclature follows Cronus
+
+% load sample data in Cronus excel format
+[num,txt,~] = xlsread('Cl_Crete_617_forCronusCalc.xlsx','sheet2');
 
 %% assign data and constants -------------------------------------------- %
 p_thickness = 2.06; 	% parameter for power law distribution of sediment layer thickness (Gomez et al., 2002)
 p_time = 1.4;		    % parameter for power law distribution of sediment layer waiting time (Gomez et al., 2002)
 e = 0;                  % erosion rate (mm/yr)
-att_neut_uncert = 0.05;	% relative uncertainty on the attenuation length for neutrons
-att_sm_uncert = 0.5;	% relative uncertainty on the attenuation length for stopping muons
-att_fm_uncert = 0.5;    % relative uncertainty on the attnunation length for fast muons
+P_neut_uncert = 0.05;	% relative uncertainty on the spallation production rate
+P_sm_uncert   = 0.3;    % relative uncertainty on the stopping muons production rate
+P_fm_uncert   = 0.3;    % relative uncertainty on the fast muons production rate
+if strcmpi(nuclide,'36Cl')
+    P_th_uncert  = 0.5; % relative uncertainty on the thermal neutron capture production rate
+    P_eth_uncert = 0.5; % relative uncertainty on the thermal neutron capture production rate
+end
+
+% Add additonal constraints on deposition ------------------------------- %
+Model = struct();
+Model.depths = [0; 500; 550];          % depths of constrained layers, first should always be 0 for surface (cm)
+Model.depths_uncerts = [0; 100; 100];
+Model.ages = [8e3; 28e3; 49e3];         % ages of constrained layers in yrs
+Model.age_uncerts = [2.5e3; 2e3; 4e3];   % age uncertainty of constrained layers in yrs
+% add constraints from main sample to additional ones
+Model.depths = [Model.depths;depth];   depth = Model.depths;
+Model.depths_uncerts = [Model.depths_uncerts; depth_uncert]; depth_uncert = Model.depths_uncerts;
+Model.ages   = [Model.ages;age];       age   = Model.ages;
+Model.age_uncerts = [Model.age_uncerts;age_uncert]; age_uncert = Model.age_uncerts;
+
 switch nuclide
     case '10Be'
-        addpath '..\..\..\..\Crete\Cretan_fans\data'
-        [data,~,raw] = xlsread('Samples_09-19.xlsx','10Be'); %load sample data
         ind = input('Enter the number of the sample you want to run (the row within the data file) ');
         depth = data(ind,11)*100;        % depth of sample (cm)
         depth_uncert = data(ind,12)*100; % uncertainty in overburden (cm)
@@ -45,31 +62,15 @@ switch nuclide
         age_uncert = data(ind,14)*1e3;  % age uncertainty (yrs)
         lat = data(ind,3);              % latitude in degree, must be same for all depths
         lon = data(ind,4);              % longitude in degree (needed for cutoff rigidity)
-        elev = data(ind,5);             % elevation in m
+        elev = data(ind,5);             % elevation in (m)
         Tshd = data(ind,15);            % topographic shielding factor
         name = raw(1+ind,1);
 
         % CONSTANTS ----------------------------------------------------- %
-        rho = 2.2;              % density g/cm³
         lambda = log(2)/1.39e6;	% decay constant for 10Be
-        air_pressure = 1013.25*exp(((-0.03417)/6.5e-3)*(log(288.15)-log(288.15-(6.5e-3*elev)))); 
         
-        att_neut = neutron_att_length(air_pressure,elev,lat,lon); % following CRONUS 2.0, Marrero 2016
-        att_sm = 1500;
-        att_fm = 4320;
-        % UNCERTAINTIES ------------------------------------------------- %
-        rho_uncert = 0.1;	    % relative uncertainty on the density
-        neutron_uncert = 0.08;	% relative uncertainty on the local neutron production rate (mainly from scaling)
-        muon_uncert = 0.08;		% relative uncertainty on the local production rate (mainly from scaling)
-        % PRODUCTION RATES ---------------------------------------------- %
-        P = DepthProd_Heisinger_Balco2017(0,lat,lon,elev,10,'calc'); % neglects tchanges in production right due to elevation changes during deposition
-        neutron = P(1);
-        muon_s = P(2);
-        muon_f = P(3);
-
     case '36Cl'
-        [data,~,~]  = xlsread('datarock_ROtest.xls',2); % load chemistry data
-        [geodata,~,raw] = xlsread('datarock_ROtest.xls',3); % load geogrpahical data (lat,elevation)
+
         ind = input('Enter the number of the sample you want to run (the row within the data file) ');
         inds = ind;
         % check if there are more samples from this section in the data
@@ -80,9 +81,7 @@ switch nuclide
 
         % update constants
         % CONSTANTS ----------------------------------------------------- %
-        rho = 2.2;              % density g/cmï¿½
         lambda = log(2)/3.013e5;% decay constant for 36Cl (Audi, 2017)
-        PsCa = 52.16;           % Ca spallation rate, (Marrero et al., 2015)
         lat = geodata(ind,1);   % sample latitude
         elev = geodata(ind,2);  % sample elvation
         name = raw(ind+1,1);    % sample name
@@ -90,15 +89,10 @@ switch nuclide
         age_uncert = geodata(inds,6)*1e3;
         depth = geodata(inds,3)*100;  % samle depth
         sample_chem = data(ind+3,1:66);% sample chemistry
-        % UNCERTAINTIES ------------------------------------------------- %
-        rho_uncert = 0.1;	    % relative uncertainty on the density
-        neutron_uncert = 0.08;	% relative uncertainty on the local neutron production rate (mainly from scaling)
-        muon_uncert = 0.08;		% relative uncertainty on the local production rate (mainly from scaling)
+
 
         % assign scaling factor for nucleonic and muonic production ----- %
         pressure = stdatm(elev);        % convert to air pressure
-        EL_f = stone2000(lat,pressure); % scaling neutrons
-        EL_mu = Sc_muons(pressure);     % scaling muons
         % Calculate depth scalings
         so_e  = exp(-(1:depth(end)) .* rho/att_neut); % depth scaling neutrons
         so_mu = exp(-(1:depth(end)) .* rho/att_sm);   % depth scaling muons
@@ -115,17 +109,22 @@ switch nuclide
         end
 end
 
-% Add additonal constraints on deposition ------------------------------- %
-Model = struct();
-Model.depths = [0];          % depths of constrained layers, first should always be 0 for surface (cm)
-Model.depths_uncerts = [0];
-Model.ages = [55e3];         % ages of constrained layers in yrs
-Model.age_uncerts = [5e3];   % age uncertainty of constrained layers in yrs
-% add constraints from main sample to additional ones
-Model.depths = [Model.depths;depth];   depth = Model.depths;
-Model.depths_uncerts = [Model.depths_uncerts; depth_uncert]; depth_uncert = Model.depths_uncerts;
-Model.ages   = [Model.ages;age];       age   = Model.ages;
-Model.age_uncerts = [Model.age_uncerts;age_uncert]; age_uncert = Model.age_uncerts;
+%% PRODUCTION RATES ----------------------------------------------------- %
+pp=physpars();
+max_depth = depth(end) + 10*depth_uncert(end); % get maximum possible depth to come up in forward models, this will e the depth until which the production profile is calculated
+switch nuclide
+    case '10Be'
+        sp = samppars1026(sampledata);        % Extract the sample parameters from the sampledatavector.
+        sf = scalefacs1026(sp,scaling_model); % get scaling factors
+        cp = comppars1026(pp,sp,sf,maxdepth); % computed parameters
+        [~,~,Ps10,Pmu10,~,~] = prodz1026(1:max_depth,pp,sf,cp);   % get production rates for a productoin profile
+    case '36Cl'
+        sp = samppars36(sampledata);        % Extract the sample parameters from the sampledatavector.
+        sf = scalefacs36(sp,scaling_model); % get scaling factors
+        cp = comppars36(pp,sp,sf,maxdepth); % computed parameters
+        [~,Ps36,~,~,~,~,Pth36,Peth36,Pmu36,~,~,~,~,~,~,~] = ...
+        prodz36(currentdepths-deltadepth/2,pp,sf,cp);             % get production rates for a productoion profile
+end
 
 
 %% START FORWARD MODELS ------------------------------------------------- %
@@ -138,13 +137,15 @@ for i = 1:n
         % RANDOM SAMPLING ----------------------------------------------- %
         % Random sampling of the different parameters from a normal distribution for each of the n realisations of the simulation.
         % The ages of the core are sampled randomely within the uncertainty bounds of the dating provided to the function in Depth_age
-        section_depth = round(normrnd(depth(end),depth_uncert(end)));  % depth in cm
+        section_depth = round(truncnormrnd(1,depth(end),depth_uncert(end),600 , 1000));  % depth in cm
+%         section_depth = round(normrnd(depth(end),depth_uncert(end)));  % depth in cm
         Depth_age_guess = [[depth(1:end-1);section_depth],round(normrnd(age,age_uncert))];	% depth, age, matrix
 
         % if there's an age inversion, take a new sample
         counter = 0;
         while any(diff(Depth_age_guess(:,2)) < 0)
-            section_depth = round(normrnd(depth(end),depth_uncert(end)));  % depth in cm
+            section_depth = round(truncnormrnd(1,depth(end),depth_uncert(end),600 , 1000));  % depth in cm
+%             section_depth = round(normrnd(depth(end),depth_uncert(end)));  % depth in cm
             Depth_age_guess = [[depth(1:end-1);section_depth],round(normrnd(age,age_uncert))];
             counter = counter + 1;
             if counter > 1e4
@@ -152,10 +153,8 @@ for i = 1:n
             end
         end
 
-        % A column of densities is produced taking into account the uncertainty
-        density_guess = normrnd(ones(1,section_depth)*rho, ones(1,section_depth)*rho*rho_uncert)';
 
-        %For attenuation length we sample from a truncated (2 sigma on both sides) normal distribution to avoid negative and extreme values
+        %For production rates we sample from a truncated (3 sigma on both sides) normal distribution to avoid negative and extreme values
         att_neut_guess = att_neut + att_neut* TruncatedGaussian(att_neut_uncert, [-2*att_neut_uncert, 2*att_neut_uncert],1);
         att_sm_guess = att_sm + att_sm* TruncatedGaussian(att_sm_uncert, [-2*att_sm_uncert, 2*att_sm_uncert],1);
         att_fm_guess = att_fm + att_fm* TruncatedGaussian(att_fm_uncert, [-2*att_fm_uncert, 2*att_fm_uncert],1);
@@ -236,7 +235,7 @@ for i = 1:n
                                 ((muon_s_guess.*exp(-tmp_depth'./att_sm_guess))./lambda).*(1-exp(-tmp_time.*lambda))+...
                                 ((muon_f_guess.*exp(-tmp_depth'./att_fm_guess))./lambda).*(1-exp(-tmp_time.*lambda));
                 case '36Cl'
-                    tmp_conc = ((P_cosmo(tmp_depth).*exp(-tmp_depth'./att_neut_guess))./lambda).*(1-exp(-tmp_time.*lambda))+ P_rad(1);
+                    tmp_conc = ((P_cosmo(tmp_depth).*exp(-tmp_depth'./att_neut_guess))./lambda).*(1-exp(-tmp_time.*lambda))+ P_rad(1); % the radiogenic production is incorrect. it also needs to be multiplied by time
             end
 
             % The result is added to the production vector
@@ -252,8 +251,9 @@ Pmean = round(mean(Production));
 Pmedian = round(median(Production));
 Pstd = round(std(Production));
 Pquantiles = round(quantile(Production,[0.17,0.83]));
-disp(['postburial production = ' num2str(Pmean) ' +/- ' num2str(Pstd)]) 
-disp(['postburial production = ' num2str(Pmedian) ' +/- ' num2str(diff(Pquantiles)/2)]) 
+disp(['postburial production ' name{1} ' = ' num2str(Pmean) ' +/- ' num2str(Pstd)]) 
+disp(['postburial production median ' name{1} ' = ' num2str(Pmedian) ' +/- ' num2str(diff(Pquantiles)/2)]) 
+disp(['postburial production quantiles ' name{1} ' = ' num2str(Pquantiles(1)) ' - ' num2str(Pquantiles(2))]) 
 
 
 %% EXPORT THE DATA
