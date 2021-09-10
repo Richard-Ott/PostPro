5cd2cf8b83d3272b1ccd50d8b96ef54343d76ade
Richard-Ott <richard.ott1900@gmail.com>
2021-09-10 14:54:26 +0200

    first running version of 10Be and 36Cl with loop

--- postburial_prod.m
+++ postburial_prod.m
@@ -16,32 +16,34 @@ addpath '.\subroutines'
 addpath '..\..\..\..\Crete\Cretan_fans\data'
 
 % USER CHOICE ----------------------------------------------------------- %
-nuclide = '36Cl';       % Choose '10Be' or '36Cl'
-n = 1e2;                % number of runs
+nuclide = '10Be';       % Choose '10Be' or '36Cl'
+n = 5e3;                % number of runs
 global scaling_model
 scaling_model = 'lm';   % choose your scaling model, nomenclature follows Cronus
 
 % load sample data in Cronus excel format
-[num,txt,~] = xlsread('36Cl_data_CRONUS.xlsx','Matlab Postburial');
-% [num,txt,~] = xlsread('10Be_data_CRONUS','Matlab Postburial');
+% [num,txt,~] = xlsread('36Cl_data_CRONUS.xlsx','Matlab Postburial');
+[num,txt,~] = xlsread('10Be_data_CRONUS','Matlab Postburial');
 % load burial histories
-[burial,burialtxt,~] = xlsread('Burialmodels','36Cl_model1');
-tag = 'model2';
+[burial,burialtxt,~] = xlsread('Burialmodels','10Be_model1');
+tag = 'test';
 
-%% assign data and constants ---------------1----------------------------- %
+inds = 1:7;
+for i = inds
+    %% assign data and constants ---------------1----------------------------- %
 
-Perr = Puncerts(nuclide); % load uncertainties for production parameters
+    Perr = Puncerts(nuclide); % load uncertainties for production parameters
 
-[Model,Para] = assignData(num,txt,burial,burialtxt,nuclide); % assign data
+    [Model,Para] = assignData(num,txt,burial,burialtxt,nuclide,inds(i)); % assign data
 
-%% PRODUCTION RATES ----------------------------------------------------- %
+    %% PRODUCTION RATES ----------------------------------------------------- %
 
-Prod = ProductionParas(Para,nuclide);
+    Prod = ProductionParas(Para,nuclide);
 
-%% RUN FORWARD MODELS --------------------------------------------------- %
+    %% RUN FORWARD MODELS --------------------------------------------------- %
 
-Model = postburial_calc(Perr,Para,Model,Prod,nuclide,n);
+    Model = postburial_calc(Perr,Para,Model,Prod,nuclide,n);
 
-%% EXPORT THE DATA
-% save(['./output/linearAggradation' nuclide '/PostburialProd_' name{1} '_' tag '.mat'], 'Model')                % save model parameters
-
+    %% EXPORT THE DATA
+    save(['./output/linearAggradation/' nuclide '/PostburialProd_' Para.name{1} '_' tag '.mat'], 'Model')                % save model parameters
+end
